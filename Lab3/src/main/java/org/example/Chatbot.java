cbf3927c3e2f58357761a3178436daa573fe3fd7
FelipeBaeza <felipe.baeza.m@usach.cl>
2023-12-04 21:29:17 -0300

    Creacion de chatbot
    selectores y modificadores

---
+++ Lab3/src/main/java/org/example/Chatbot.java
@@ -0,0 +1,74 @@
+package org.example;
+import java.util.List;
+public class Chatbot {
+
+    int chatbotID;
+    String name;
+    String welcomeMessage;
+    int startFlowId;
+    List<Flow> flows;
+
+    public Chatbot(int chatbotID, String name, String welcomeMessage, int startFlowId, List<Flow> flows) {
+        this.chatbotID = chatbotID;
+        this.name = name;
+        this.welcomeMessage = welcomeMessage;
+        this.startFlowId = startFlowId;
+        this.flows = flows;
+    }
+
+    public int getChatbotID() {
+        return chatbotID;
+    }
+
+    public void setChatbotID(int chatbotID) {
+        this.chatbotID = chatbotID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWelcomeMessage() {
+        return welcomeMessage;
+    }
+
+    public void setWelcomeMessage(String welcomeMessage) {
+        this.welcomeMessage = welcomeMessage;
+    }
+
+    public int getStartFlowId() {
+        return startFlowId;
+    }
+
+    public void setStartFlowId(int startFlowId) {
+        this.startFlowId = startFlowId;
+    }
+
+    public List<Flow> getFlows() {
+        return flows;
+    }
+
+    public void setFlows(List<Flow> flows) {
+        this.flows = flows;
+    }
+
+    public void chatbotAddFlow(Flow flow){
+        // filtro de flows
+        this.flows.add(flow);
+    }
+
+    @Override
+    public String toString() {
+        return "Chatbot{" +
+                "chatbotID=" + chatbotID +
+                ", name='" + name + '\'' +
+                ", welcomeMessage='" + welcomeMessage + '\'' +
+                ", startFlowId=" + startFlowId +
+                ", \nflows=" + flows +
+                '}';
+    }
+}
